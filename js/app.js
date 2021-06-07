f259c37a7632d8879485de58ef169e7213e63b30
reconrex <76620859+reconrex@users.noreply.github.com>
2021-06-08 00:22:01 +0600

    try to convert srt

--- js/app.js
+++ js/app.js
@@ -890,7 +890,76 @@ function file_code(path) {
 function file_video(path) {
     var name = path.split('/').pop();
     var decodename = unescape(name);
-    var caption = name.slice(0, name.lastIndexOf('.'))
+    var caption = name.slice(0, name.lastIndexOf('.srt'))
+    var captionx = srt2webvtt(caption);
+
+    function srt2webvtt(data) {
+      // remove dos newlines
+      var srt = data.replace(/\r+/g, '');
+      // trim white space start and end
+      srt = srt.replace(/^\s+|\s+$/g, '');
+
+      // get cues
+      var cuelist = srt.split('\n\n');
+      var result = "";
+
+      if (cuelist.length > 0) {
+        result += "WEBVTT\n\n";
+        for (var i = 0; i < cuelist.length; i=i+1) {
+          result += convertSrtCue(cuelist[i]);
+        }
+      }
+
+      return result;
+    }
+
+    function convertSrtCue(caption) {
+      // remove all html tags for security reasons
+      //srt = srt.replace(/<[a-zA-Z\/][^>]*>/g, '');
+
+      var cue = "";
+      var s = caption.split(/\n/);
+
+      // concatenate muilt-line string separated in array into one
+      while (s.length > 3) {
+          for (var i = 3; i < s.length; i++) {
+              s[2] += "\n" + s[i]
+          }
+          s.splice(3, s.length - 3);
+      }
+
+      var line = 0;
+
+      // detect identifier
+      if (!s[0].match(/\d+:\d+:\d+/) && s[1].match(/\d+:\d+:\d+/)) {
+        cue += s[0].match(/\w+/) + "\n";
+        line += 1;
+      }
+
+      // get time strings
+      if (s[line].match(/\d+:\d+:\d+/)) {
+        // convert time string
+        var m = s[1].match(/(\d+):(\d+):(\d+)(?:,(\d+))?\s*--?>\s*(\d+):(\d+):(\d+)(?:,(\d+))?/);
+        if (m) {
+          cue += m[1]+":"+m[2]+":"+m[3]+"."+m[4]+" --> "
+                +m[5]+":"+m[6]+":"+m[7]+"."+m[8]+"\n";
+          line += 1;
+        } else {
+          // Unrecognized timestring
+          return "";
+        }
+      } else {
+        // file format error or comment lines
+        return "";
+      }
+
+      // get cue text
+      if (s[line]) {
+        cue += s[line] + "\n\n";
+      }
+
+      return cue;
+    }
     var path = path;
     var url = UI.second_domain_for_dl ? UI.downloaddomain + path : window.location.origin + path;
     var url_without_https = url.replace(/^(https?:|)\/\//,'')
@@ -913,7 +982,7 @@ function file_video(path) {
 	<video id="vplayer" width="100%" height="100%" playsinline controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen']; data-plyr-config="{ "title": "${decodename}"}" data-poster="${poster}" style="--plyr-captions-text-color: #ffffff;--plyr-captions-background: #000000;">
 	  <source src="${url}" type="video/mp4" />
 	  <source src="${url}" type="video/webm" />
-	  <track kind="captions" label="Default" src="${caption}.srt" srclang="en" />
+	  <track kind="captions" label="Default" src="${captionx}.srt" srclang="en" />
     <track kind="captions" label="English" src="${caption}.srt" srclang="en" default />
     <track kind="captions" label="Hindi" src="${caption}.hi.vtt" srclang="hi" />
     <track kind="captions" label="Russian" src="${caption}.ru.vtt" srclang="ru" />
