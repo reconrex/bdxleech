ef920eca697d65e58a108f5e5a95b8330a4436a3
reconrex <76620859+reconrex@users.noreply.github.com>
2021-06-08 00:24:59 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -982,8 +982,8 @@ function file_video(path) {
 	<video id="vplayer" width="100%" height="100%" playsinline controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen']; data-plyr-config="{ "title": "${decodename}"}" data-poster="${poster}" style="--plyr-captions-text-color: #ffffff;--plyr-captions-background: #000000;">
 	  <source src="${url}" type="video/mp4" />
 	  <source src="${url}" type="video/webm" />
-	  <track kind="captions" label="Default" src="${captionx}.srt" srclang="en" />
-    <track kind="captions" label="English" src="${caption}.srt" srclang="en" default />
+	  <track kind="captions" label="Default" src="${caption}.srt" srclang="en" />
+    <track kind="captions" label="English" src="${captionx}" srclang="en" default />
     <track kind="captions" label="Hindi" src="${caption}.hi.vtt" srclang="hi" />
     <track kind="captions" label="Russian" src="${caption}.ru.vtt" srclang="ru" />
     <track kind="captions" label="Malayalam" src="${caption}.ml.vtt" srclang="ml" />
